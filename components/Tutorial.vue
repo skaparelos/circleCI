82dda06918dbfbeed6ce30b0e9bc1f94b285e855
Stavros Kaparelos <skaparelos@gmail.com>
2022-04-07 19:03:49 +0300

    changes tutorial

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -55,7 +55,7 @@
           Welcome to your Nuxt Application
         </h2>
         <p class="mt-3 text-gray-600">
-          We recommend you take a look at the test page here:
+          We recommend you take a look at the test page here:sdihjasiuhd
           <a
             href="https://nuxtjs.org"
             target="_blank"
